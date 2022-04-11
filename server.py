5f746e71f56001ad4eb8f3d1493f0af906d7e5fe
adrianhuber17 <adrianhuber17@gmail.com>
2022-04-10 18:06:40 -0700

    updated gitignore, server.py to add secretapp key

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify, session
+import os
 from gpx_parser import gpxParser
 
 app = Flask(__name__)
+SECRET_KEY = os.environ['APP_KEY']
+app.secret_key = SECRET_KEY
 
 
 @app.route('/')
 def index():
+    """homepage route"""
 
 
     return render_template('index.html')
 
 @app.route("/map.json")
 def get_activity_map_data():
-    """"""
+    """endpoint with cordinates JSON for the map"""
     gpx_file_test = 'Camilo_Ride.gpx'
     activity = gpxParser()
     activity.complete_gpx_parser_main(gpx_file_test)
