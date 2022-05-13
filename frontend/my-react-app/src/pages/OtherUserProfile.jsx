27642e790bfc32dfa681db953d5ec44c1b6393e7
adrianhuber17 <adrianhuber17@gmail.com>
2022-05-12 18:19:24 -0700

    added map to other user profile, and added profile information.

--- frontend/my-react-app/src/pages/OtherUserProfile.jsx
+++ frontend/my-react-app/src/pages/OtherUserProfile.jsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react";
 import { GetOtherUserId } from "../helperFunction/StateParamWrapper";
+import ActivityMap from "../components/ActivityMap";
+import OtherUserTable from "../components/OtherUserTable";
 
 const OtherUser = () => {
-  const otherUserId = GetOtherUserId();
-  console.log(otherUserId);
-  return <h1>Other User Profile : {otherUserId}</h1>;
+  const [centerLatitude, setCenterLatitude] = useState("");
+  const [centerLongitude, setCenterLongitude] = useState("");
+  const [coordinates, setCoordinates] = useState([""]);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const userId = GetOtherUserId();
+
+  useEffect(() => {
+    const data = { userId: userId };
+    fetch("/other-user.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((respData) => {
+        console.log(respData);
+        if (respData.userLatestRide !== null) {
+          setCenterLatitude(respData.userLatestRide.latitude);
+          setCenterLongitude(respData.userLatestRide.longitude);
+          setCoordinates(respData.userLatestRide.coordinates);
+          setUserData(respData.userData);
+          setIsFollowing(respData.isFollowing);
+          setLoading(false);
+        } else {
+          setCenterLatitude("37.773972");
+          setCenterLongitude("-122.431297");
+          setCoordinates(["37.773972", "-122.431297"]);
+          setUserData(respData.userData);
+          setIsFollowing(respData.isFollowing);
+          setLoading(false);
+        }
+      });
+  }, [userId]);
+
+  return (
+    <div>
+      <OtherUserTable userData={userData} />
+      {!loading && (
+        <ActivityMap
+          centerLatitude={centerLatitude}
+          centerLongitude={centerLongitude}
+          coordinates={coordinates}
+        />
+      )}
+    </div>
+  );
 };
 export default OtherUser;
