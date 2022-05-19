e5b0826ee5dd8e0439ae6d509ad3607d943ca155
adrianhuber17 <adrianhuber17@gmail.com>
2022-05-19 14:53:40 -0700

    added button for friend feed changing elevation units

--- frontend/my-react-app/src/components/FriendFeed.jsx
+++ frontend/my-react-app/src/components/FriendFeed.jsx
@@ -5,47 +5,32 @@ import ActivityMap from "./ActivityMap";
 export default function FriendFeed(props) {
   const [changeElevation, setChangeElevation] = useState(true);
 
-  /**
-   * useEffect
-   * takes length of props.friendsData and initializes userElevationUnits dictionary
-   * props.friendsData.forEach( add index: 'feet')
-   */
-  const userElevationUnits = {
-    1: "feet",
-    2: "feet",
-    3: "feet",
-  };
-
   const handleElevationChangeMeters = (event) => {
-    setChangeElevation(false);
-  };
-  const handleElevationChangeFeet = (event) => {
-    setChangeElevation(true);
+    if (changeElevation === true) {
+      setChangeElevation(false);
+    } else {
+      setChangeElevation(true);
+    }
   };
 
   return (
     <>
       <h1>Friends Feed</h1>
+      <button onClick={handleElevationChangeMeters}>Change Units</button>
       {props.friendsData.map((activity, ind) => (
         <div key={ind}>
           <h2>{`${activity.firstName} ${activity.lastName}`}</h2>
           <p>{activity.date}</p>
           <p>{activity.rideCaption}</p>
-          {userElevationUnits[ind] === "meters" ? (
+          {changeElevation ? (
             <>
               <p>
                 {`Elevation gain: ${activity.elevationGainLossJson.elevation_gain_feet} feet`}
               </p>
-              <button value={ind} onClick={handleElevationChangeMeters(ind)}>
-                Change to meters
-              </button>
             </>
           ) : (
             <>
               <p>{`Elevation gain: ${activity.elevationGainLossJson.elevation_gain_meters} meters`}</p>
-              <button value={ind} onClick={handleElevationChangeFeet}>
-                Change to feet
-              </button>
             </>
           )}
 
