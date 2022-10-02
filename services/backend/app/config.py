712ca7cb1678cc23cf49cfc99c766c8ac3b4270e
adrianhuber17 <adrianhuber17@gmail.com>
2022-10-02 16:50:26 -0700

    API configs

--- services/backend/app/config.py
+++ services/backend/app/config.py
@@ -3,50 +3,23 @@
 import os
 
 class BaseConfig:
-    FLASK_ENV='development'
-    DEBUG = False
+    DEBUG = True
     TESTING = False
-    SECRET_KEY = 'secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
-    FLASK_RUN_PORT=5001
+    SECRET_KEY = "secret"  # new
+    SESSION_COOKIE_HTTPONLY=True
+    REMEMBER_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_SAMESITE="Lax"
+    
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///fitness_app'
-    print(SQLALCHEMY_DATABASE_URI)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
-
 class ProductionConfig(BaseConfig):
-    url = os.environ.get("DATABASE_URL")
-
-    if url is not None and url.startswith("postgres://"):
-        url = url.replace("postgres://", "postgresql://", 1)
-
-    SQLALCHEMY_DATABASE_URI = url
-
-class BaseConfigSocket:
-    FLASK_ENV='development'
-    DEBUG = False
-    TESTING = False
-    SECRET_KEY = 'secret'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
-
-
-class DevelopmentConfig(BaseConfigSocket):
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///fitness_app'
-    print(SQLALCHEMY_DATABASE_URI)
-
-class TestingConfig(BaseConfigSocket):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
-
-
-class ProductionConfig(BaseConfigSocket):
     url = os.environ.get("DATABASE_URL")
 
     if url is not None and url.startswith("postgres://"):
--- services/backend/manage.py
+++ services/backend/manage.py
@@ -3,6 +3,7 @@
 import sys, os
 from flask.cli import FlaskGroup
 from app import create_app,db
+from app.api import crud
 
 
 app = create_app()
@@ -15,6 +16,16 @@ def recreate_db():
     db.create_all()
     db.session.commit()
     print("database reset done!")
+
+@cli.command('populate_db')
+def populate_db():
+    new_user = crud.create_user("ad@gmail.com",
+                                "adrian",
+                                "huber",
+                                "123")
+    db.session.add(new_user)
+    db.session.commit()
+    print("user_created")
 
     
 if __name__ == "__main__":
