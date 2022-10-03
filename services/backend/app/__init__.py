cccfe8f19bc0ef04fe41b11d6e71d6229a22a62d
adrianhuber17 <adrianhuber17@gmail.com>
2022-10-02 17:03:28 -0700

    init py

--- services/backend/app/__init__.py
+++ services/backend/app/__init__.py
@@ -5,24 +5,27 @@ from flask import Flask,request,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_socketio import SocketIO,emit
-import app.config as config
 
 db = SQLAlchemy()
 cors = CORS()
 socketio = SocketIO()
 
 def create_app(script_info=None):
+    app_settings = os.getenv("APP_SETTINGS")
 
     #instantiate app
     app = Flask(__name__)
    
     #set config
-    app.config.from_object(config.DevelopmentConfig)
+    app.config.from_object(app_settings)
+    cors.init_app(
+    app,
+    resources={r"*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}},
+    supports_credentials=True,
+    )
     
     #set up extensions
     db.init_app(app)
-    cors.init_app(app,resources={r"/*":{"origins":"*"}})
-        
     #register api
     from app.api import api
     api.init_app(app)
