2ed25be69bda50436002a29f7294d09705b36541
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-01 11:29:37 -0700

    updated crud - to delete getting user id by email. updated events to get friends new feed with websocket sid as the key. and updated manage socket to delete old events no longer needed in that file.

--- services/websocket/app/crud.py
+++ services/websocket/app/crud.py
@@ -5,14 +5,6 @@ from app.model import User, Activity
 from sqlalchemy import desc
 #CRUD helper functions
 
-def get_user_id(email):
-    """get a User id"""
-
-    user = db.session.query(User).filter_by(email=email).one()
-    user_id = user.user_id
-
-    return user_id
-
 def get_user_followers(user_id):
     """return user's follower list"""
     user_obj = db.session.query(User).filter_by(user_id = user_id).one()
--- services/websocket/app/events.py
+++ services/websocket/app/events.py
@@ -32,3 +32,12 @@ def test_disconnected():
     print("current online users: ",users_online)
     emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
+@socketio.on("new_data")
+def new_data(data):
+    follower_info_list = get_user_followers(data)
+    for follower in follower_info_list:
+        for sid,user_id in users_online.items():
+            if follower['userId'] != user_id:
+                continue
+            follower_sid = sid
+            emit("new_data",{'count':1},to=follower_sid,include_self=False)
